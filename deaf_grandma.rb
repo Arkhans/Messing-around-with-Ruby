d5cb39ef800722ee85af14d7d13f3bd7229d20d6
Greg Kavvadias <gregkavvadias@gmail.com>
2016-02-03 19:51:32 +0200

    fixes

--- deaf_grandma.rb
+++ deaf_grandma.rb
@@ -5,12 +5,10 @@ while bye < 3
   answer = gets.chomp
     if answer != answer.upcase
     puts "HUH?!  SPEAK UP, SONNY!"
-    bye = 0
     end
     if answer == answer.upcase
       year = rand(1930..1950)
       puts "NO, NOT SINCE " + year.to_s + "!"
-      bye = 0
     end
     if answer == "BYE" || answer == "BYE!"
       bye += 1
